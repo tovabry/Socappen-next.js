be9c6937796141203d4eb904ea3bf83a556579a5
Tova <tova.bryngelsson@live.se>
2026-03-30 09:44:06 +0200

    posts setup

--- app/question/pending/page.tsx
+++ app/question/pending/page.tsx
@@ -11,15 +11,16 @@ interface ResponseQuestion {
 export default function PendingPage() {
   const [questions, setquestions] = useState<ResponseQuestion[]>([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8080/api/question/pending")
-      .then(res => res.json())
-      .then((data: ResponseQuestion[]) => setquestions(data))
-      .catch(err => {
-        console.error("Failed to fetch questions", err);
-        setquestions([]);
-      });
-  }, []);
+useEffect(() => {
+    fetch("http://localhost:8080/api/question/pending", {
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+    })
+    .then(res => res.json())
+    .then((data: ResponseQuestion[]) => setquestions(data))
+    .catch(err => console.error("Failed to fetch questions", err));
+}, []);
 
   return (
     <div>
