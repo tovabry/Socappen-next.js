1b2946bf9e4fc3fdf5f61fc7c9aff6ce5ecf54cf
Victor Sundberg <victor.sundberg@herrljunga.se>
2026-04-28 09:03:39 +0200

    Updated footer ui with icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,13 @@
+import { Globe, Mail } from "lucide-react";
+
 export function Footer() {
 	return (
 		<footer className="bg-(--bg-secondary-color-red) text-white py-4 shadow-lg mt-4 overflow-hidden">
 			<div className="flex flex-col md:flex-row md:justify-around justify-between px-8 gap-4">
 				<section className="flex flex-col min-w-0 text-start">
-					<h3 className="text-lg mb-2">Om oss</h3>
+					<h3 className="text-lg mb-2 underline underline-offset-2 ">Om oss</h3>
 					<p className="text-md mb-2">Resursenheten hanterar frågor om:</p>
-					<ul className="list-disc list-inside text-md">
+					<ul className="list-disc list-inside text-sm">
 						<li>Familjefrågor</li>
 						<li>Ungdomsfrågor</li>
 						<li>Stöd och rådgivning</li>
@@ -13,24 +15,25 @@ export function Footer() {
 				</section>
 
 				<section className="flex flex-col min-w-0 text-start gap-1">
-					<h3 className="text-lg mb-2">Kontakt</h3>
+					<h3 className="text-lg underline underline-offset-2 mb-2">Kontakt</h3>
 					<p className="text-md">Öppettider: 10:00 - 16:00</p>
 					<p className="text-md">Telefon: 0322-000 00</p>
 					<a
 						href="mailto:resursenheten@herrljunga.se"
-						className="text-md text-white underline underline-offset-2"
+						className="text-md text-white"
 						aria-label="Länk till mail-adress"
 					>
-						resursenheten@herrljunga.se
+						<Mail size={16} className="inline-block" />{" "}
+						Resursenheten@herrljunga.se
 					</a>
 					<a
-						href="#"
-						className="text-md text-white underline underline-offset-2"
+						href="https://www.exempel.se"
+						className="text-md text-white"
 						target="_blank"
 						rel="noopener noreferrer"
 						aria-label="Besök Exempel webbplats"
 					>
-						www.exempel.se
+						<Globe size={16} className="inline-block" /> www.exempel.se
 					</a>
 				</section>
 			</div>
